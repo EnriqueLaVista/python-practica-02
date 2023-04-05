7cb7c1c21e33069becb4c1f89fb2db9489470cf2
Enrique La Vista <enriquelavistaolivera@gmail.com>
2023-04-05 17:23:30 -0300

    Resolución de inciso A

--- E10.py
+++ E10.py
@@ -16,3 +16,23 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
 64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
 95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
 
+# Inciso A
+
+# Limpiar el string de nombres
+for char in [' ', "'", '\n']:
+    nombres = nombres.replace(char,'')
+
+# Crear diccionario de nombres con las notas
+lista_alumnos = list(zip(nombres.split(','), notas_1, notas_2))
+
+dict_alumnos = {alum[0]: [alum[1], alum[2]] for alum in lista_alumnos}
+
+# Inciso B
+
+
+
+# Inciso C
+
+# Inciso D
+
+# Inciso E
